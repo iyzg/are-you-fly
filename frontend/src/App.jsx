34d6f489cfcd9406c1e927bb57e379c5d9439e81
Ivy Zhang <zivy042003@gmail.com>
2024-08-03 20:55:45 -0400

    soooo simple frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,30 +4,50 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
+  const [resText, setResText] = useState('')
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    console.log('submitted: ', text)
+
+    try {
+      const res = await fetch('http://howflyareyou.com/api/novelty', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text })
+      })
+
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await res.json()
+      console.log('returned novelty: ', data.result)
+      setResText('Novelty: ' + data.result)
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  }
+
+  const handleTextChange = (event) => {
+    setText(event.target.value)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>How fly are you?</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          text: <input value={text} onChange={handleTextChange} />
+        </div>
+        <div>
+          <button type="submit">submit</button>
+        </div>
+        <p>{resText}</p>
+      </form>
     </>
   )
 }
