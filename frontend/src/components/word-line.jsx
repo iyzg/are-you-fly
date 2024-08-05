c7e86c5fdc9ff498d1cf6fcad28b9f1606056bea
Ivy Zhang <zivy042003@gmail.com>
2024-08-05 17:51:05 -0400

    fix bar scaling issue

--- frontend/src/components/word-line.jsx
+++ frontend/src/components/word-line.jsx
@@ -1,7 +1,7 @@
 const WordLine = ({ word, score }) => {
     return (
         <>
-            <p className='top-word' style={{width: `calc(${score * 0.8} * 100%)`}}>{word} { }</p>
+            <p className='top-word' style={{width: `calc(${score / 100 * 0.8} * 100%)`}}>{word} { }</p>
             
         </>
     )
