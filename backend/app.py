952d86ad52bdeba1ced8d2b25b4340f001a3c7c3
Ivy Zhang <zivy042003@gmail.com>
2024-08-05 17:48:05 -0400

    tuned eps to be tough to get fully novel

--- backend/app.py
+++ backend/app.py
@@ -6,17 +6,21 @@ from sentence_transformers import SentenceTransformer
 
 
 class SimilarityStoreApp():
-    def __init__(self, store_dim=5000, top_k=10, ones_per_col=20, eps=0.05):
+    def __init__(self, store_dim=5000, top_k=10, ones_per_col=20):
         self.store_dim = store_dim
         self.embed_dim = 384
 
         self.stored_embeds = np.ones(store_dim)
+        # NOTE: k is like number of hash functions from bloom filter
         self.top_k = top_k
         self.model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+        # NOTE: Random projects act like random hash functions
         self.random_proj = np.zeros((self.embed_dim, store_dim))
         self.rng = np.random.default_rng()
 
-        self.eps = eps
+        # Calculate how long to recover
+        self.storage_capacity = store_dim / top_k
+        self.eps = 1.0 / 1.05 * self.storage_capacity
 
         # Turn it into sparse, binary matrix with some number of 1s per column
         cols = np.repeat(np.arange(0, store_dim, dtype=np.int32), ones_per_col)
