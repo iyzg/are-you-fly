76351da5e191c9585df571a6ae563674c90850b4
Ivy Zhang <zivy042003@gmail.com>
2024-08-06 08:43:00 -0400

    update about

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -10,8 +10,10 @@ const About = (props) => {
                 How Fly Are You? uses an embedding model and   
                 &nbsp;<a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6304992/">fly-inspired bloom filter</a>&nbsp; 
                 to score the creativity of your text. Over time, things that were once
-                "boring" become novel again to our digital fly. <strong>Just how creative can
-                you be compared to the rest of the internet?</strong>
+                "boring" become novel again to our digital fly. You can submit any text, and the fly will return a score between 0 (least creative) to 100 (most creative)!
+            </p>
+            <p>
+                <strong>How &ldquo;fly&rdquo; are you compared to the rest of the internet?</strong>
             </p>
             <img src={flySVG} className='fly-logo'/>
         </div>
